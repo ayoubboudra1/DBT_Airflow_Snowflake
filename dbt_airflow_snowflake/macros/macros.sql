8859738df3f8d604c6e30ef9b429b5eaee5501e9
Ayoub boudra <ayoub.boudra@napta.io>
2025-07-26 14:20:31 +0200

    final version

--- dbt_airflow_snowflake/macros/macros.sql
+++
@@ -1,7 +0,0 @@
-{% macro discount_amount(price, discount) %}
-    ({{ price }} * {{ discount }})::numeric(16, 2)
-{% endmacro %}
-
-{% macro delivery_days(receipt_date_col, order_date_col) %}
-    ({{ receipt_date_col }} - {{ order_date_col }})::integer
-{% endmacro %}
